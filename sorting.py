e2cf47f6e5d7fbe177fe60d9dd10180713eb8fd8
Lucia Luleiova <283177@vutbr.cz>
2026-04-22 09:23:10 +0200

    bubble sort

--- sorting.py
+++ sorting.py
@@ -15,10 +15,22 @@ def selection_sort(numbers):
         numbers[j], numbers[min_index] = numbers[min_index], numbers[j]
         print(numbers)
     return numbers
+def bubble_sort(values):
+    values = values.copy()
+    for i in range(len(values) - 1 ):
+        swapped = False
+        for j in range(0, (len(values) - 1 - i)):
+            if values[j] > values[j + 1]:
+                values[j], values[j +1 ] = values[j + 1], values[j]
+                swapped = True
+        if not swapped:
+            break
+    return values
 
 if __name__ == "__main__":
     values = random_numbers(10)  # 10 čísel v rozsahu 0–100
     print(values)  # např. [42, 7, 91, 15, 63, 8, 57, 73, 2, 100]
 
     small = random_numbers(5, low=0, high=20)  # 5 čísel v rozsahu 0–20
-    print(selection_sort(values))
+    #print(selection_sort(values))
+    print(bubble_sort(values))
