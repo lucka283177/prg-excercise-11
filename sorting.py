b9627caa276fb2a8a7814668a38ddbdf35af48d5
Lucia Luleiova <283177@vutbr.cz>
2026-04-22 09:45:03 +0200

    vizualizacia bubble sortu

--- sorting.py
+++ sorting.py
@@ -1,4 +1,5 @@
 import random
+import matplotlib.pyplot as plt
 
 def random_numbers(count, low=0, high=100):
     return [random.randint(low, high) for _ in range(count)]
@@ -16,6 +17,8 @@ def selection_sort(numbers):
         print(numbers)
     return numbers
 def bubble_sort(values):
+    plt.ion()
+    plt.show()
     values = values.copy()
     for i in range(len(values) - 1 ):
         swapped = False
@@ -23,8 +26,19 @@ def bubble_sort(values):
             if values[j] > values[j + 1]:
                 values[j], values[j +1 ] = values[j + 1], values[j]
                 swapped = True
+            index_highlight1 = j
+            index_highlight2 = j + 1
+            colors = ["steelblue"] * len(values)
+            colors[index_highlight1] = "tomato"
+            colors[index_highlight2] = "tomato"
+            plt.clf()
+            plt.bar(range(len(values)), values, color=colors)
+            plt.title("Bubble Sort")
+            plt.pause(0.1)
         if not swapped:
             break
+    plt.ioff()
+    plt.show()
     return values
 
 if __name__ == "__main__":
